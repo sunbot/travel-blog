16ea89cdb39fb6a79873c7802b727e9aa9cbfb24
Angela Moyler <angelamoyler@Angelas-MBP.attlocal.net>
2019-11-11 21:04:45 -0600

    Reset to last stable

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,29 +1,22 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
-import config from '../utils/siteConfig'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import Container from '../components/Container'
-import PageBody from '../components/PageBody'
-import TagList from '../components/TagList'
-import PostLinks from '../components/PostLinks'
-import PostDetails from '../components/PostDetails'
-import SEO from '../components/SEO'
+import React from "react";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+import config from "../utils/siteConfig";
+import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import PageBody from "../components/PageBody";
+import TagList from "../components/TagList";
+import PostLinks from "../components/PostLinks";
+import PostDetails from "../components/PostDetails";
+import SEO from "../components/SEO";
 
 const PostTemplate = ({ data, pageContext }) => {
-  const {
-    title,
-    slug,
-    heroImage,
-    body,
-    publishDate,
-    tags,
-  } = data.contentfulPost
-  const postNode = data.contentfulPost
+  const { title, slug, heroImage, body, publishDate, tags } = data.Post;
+  const postNode = data.Post;
 
-  const previous = pageContext.prev
-  const next = pageContext.next
+  const previous = pageContext.prev;
+  const next = pageContext.next;
 
   return (
     <Layout>
@@ -32,7 +25,7 @@ const PostTemplate = ({ data, pageContext }) => {
       </Helmet>
       <SEO pagePath={slug} postNode={postNode} postSEO />
 
-      <Hero title={title} image={heroImage} height={'50vh'} />
+      <Hero title={title} image={heroImage} height={"50vh"} />
 
       <Container>
         {tags && <TagList tags={tags} />}
@@ -44,8 +37,8 @@ const PostTemplate = ({ data, pageContext }) => {
       </Container>
       <PostLinks previous={previous} next={next} />
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query($slug: String!) {
@@ -84,6 +77,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default PostTemplate
+export default PostTemplate;
