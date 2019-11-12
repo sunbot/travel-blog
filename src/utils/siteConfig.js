359a4584eedd04359a80795ee012864f973daa69
Angela Moyler <angelamoyler@Angelas-MBP.attlocal.net>
2019-11-11 20:44:27 -0600

    Update site config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -10,9 +10,9 @@ module.exports = {
   authorUrl: "https://hopskipjumpaway/about/", // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: "@twitter", // Change for Twitter Cards
   shortTitle: "Hop Skip Jump", // Used for App manifest e.g. Mobile Home Screen
-  shareImage: "/logos/share.jpg", // Open Graph Default Share Image. 1200x1200 is recommended
-  shareImageWidth: 900, // Change to the width of your default share image
-  shareImageHeight: 600, // Change to the height of your default share image
+  shareImage: "/logos/share.png", // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 650, // Change to the width of your default share image
+  shareImageHeight: 433, // Change to the height of your default share image
   siteLogo: "/logos/logo-512.png", // Logo used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#121212", // Used for Offline Manifest
